49a7966fcd8f5d5b2b45373fd19174a1814890ed
Vivek Kumar <93335147+vivekkumar592@users.noreply.github.com>
2025-09-13 17:08:14 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -20,6 +20,7 @@ from dotenv import load_dotenv
 api_key=os.environ.get("OPENAI_API_KEY")
 rapid_key = os.environ.get("RAPIDAPI_KEY")
 
+print("OPEN API",api_key);
 os.environ["OPENAI_API_KEY"] = api_key
 os.environ["RAPIDAPI_KEY"] = rapid_key
 
