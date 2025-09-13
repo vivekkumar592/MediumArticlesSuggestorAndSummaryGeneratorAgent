39adc4c4bd898c08c600c0ea857acd076e95ef75
Vivek Kumar <93335147+vivekkumar592@users.noreply.github.com>
2025-09-13 11:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from agent import agent
 app = FastAPI()
+import logging
 
-
-
+logger = logging.getLogger("uvicorn.error")
   # stream_graph_updates(user_input)
 
 @app.get("/recommend/{user_input}")
 async def read_user_input(user_input: str):
     agent_content = await agent(user_input)
+    logger.info(f"recomment path hit by this user request{user_input}")
     return {"agent_content": agent_content}
