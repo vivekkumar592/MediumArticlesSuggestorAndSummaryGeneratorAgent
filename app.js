760ad4afc122a816b3333f8c13489a4111117f87
Vivek Kumar <93335147+vivekkumar592@users.noreply.github.com>
2025-09-14 13:41:51 +0530

    updated agent api call from http to https

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const btnText = findButton.querySelector('.btn-text');
 const btnLoader = findButton.querySelector('.btn-loader');
 
 // API Configuration
-const API_BASE_URL = 'http://13.53.92.124:8000/recommend/';
+const API_BASE_URL = 'https://n6pj8blr56.execute-api.eu-north-1.amazonaws.com/recommend/';
 
 // State Management
 let isLoading = false;
